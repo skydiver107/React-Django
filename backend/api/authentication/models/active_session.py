e213b02e8cdca1a9fd90463c49efa78d0d9f13a4
globalhunter <rlalimurung@gmail.com>
2023-10-01 23:55:18 -0400

    first init

---
+++ backend/api/authentication/models/active_session.py
@@ -0,0 +1,8 @@
+from django.db import models
+from api.user.models import User
+
+
+class ActiveSession(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
---
+++ backend/api/authentication/viewsets/login.py
@@ -0,0 +1,21 @@
+import logging
+from rest_framework import viewsets, mixins
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+
+from api.authentication.serializers import LoginSerializer
+
+
+class LoginViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
+    permission_classes = (AllowAny,)
+    serializer_class = LoginSerializer
+    logger = logging.getLogger(__name__)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        # print("I am here.")
+
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
